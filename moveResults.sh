4102a01e9657babd981406f6c9aa9897ec22fd83
legraina <antoine.legrain@gmail.com>
2015-05-18 11:07:41 -0400

    AL: after merged

---
+++ moveResults.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for entry in "outfiles/Competition/"*
+do
+#	mv ${entry}/log.txt ${entry}/OptLog.txt 
+#	mv ${entry}/validatorOutput.txt ${entry}/OptValidatorOutput.txt
+rm ${entry}/.dropbox.attr
+done
