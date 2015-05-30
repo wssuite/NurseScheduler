3869089805a0f83cd46e3bb81139d90a39c38fde
rosat <samuel.rosat@gmail.com>
2015-05-29 21:50:16 -0400

    vcn

--- src/StochasticSolver.cpp
+++ src/StochasticSolver.cpp
@@ -32,6 +32,9 @@ options_(options), demandHistory_(demandHistory), pReusableGenerationSolver_(0),
 	options_.generationParameters_.maxSolvingTimeSeconds_ = options_.totalTimeLimitSeconds_;
 	options_.generationParameters_.weekIndices_ = { pScenario_->thisWeek() };
 
+	options.generationParameters_ = options.verbose_;
+	options.evaluationParameters_ = options.verbose_;
+
 	bestScore_ = LARGE_SCORE;
 	bestSchedule_ = -1;
 	nGenerationDemands_ = 0;
